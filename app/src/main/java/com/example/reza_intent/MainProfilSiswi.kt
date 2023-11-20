9235e68136160fe49c6e7cce1363fbc16dc8b70e
uswtnnisa <uswatunnisa382@gmail.com>
2023-11-20 13:24:39 +0700

    Initial commit

---
+++ app/src/main/java/com/example/reza_intent/MainProfilSiswi.kt
@@ -0,0 +1,11 @@
+package com.example.reza_intent
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainProfilSiswi : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_profil_siswi)
+    }
+}
---
+++ app/src/main/java/com/example/reza_intent/MainSekolah.kt
@@ -0,0 +1,11 @@
+package com.example.reza_intent
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainSekolah : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_sekolah)
+    }
+}
